1bc006d790e4505a1fe3db26cdae1f3d03dbd76f
bobby569 <bobby.yczh@gmail.com>
2018-07-14 21:45:26 -0400

    Done 2d, 2e

--- parser.py
+++ parser.py
@@ -1,19 +1,39 @@
 import util
 
 class Parser:
-    
+
     def __init__(self, filename):
         self.filename = filename
+        self.word_dict = None
+
+
+    def getWordDict(self):
+        if self.word_dict:
+            return self.word_dict
+
+        txt = open(self.filename, "r")
+
+        word_dict = {}
+        for line in txt:
+            word_list = util.getWordList(line)
+            for word in word_list:
+                word_dict[word] = word_dict.get(word, 0) + 1
+
+        txt.close()
+        self.word_dict = word_dict
+        return word_dict
+
 
     def getTotalNumberOfWords(self):
         txt = open(self.filename, "r")
         cnt = 0
-        
+
         for line in txt:
             cnt += len(util.getWordList(line))
 
-        txt.close()                
+        txt.close()
         return cnt
+
 
     def getTotalUniqueWords(self):
         txt = open(self.filename, "r")
@@ -27,14 +47,9 @@ class Parser:
         txt.close()
         return len(word_set)
 
-    def get20MostFrequentWords(self):
-        txt = open(self.filename, "r")
-        word_dict = {}
 
-        for line in txt:
-            word_list = util.getWordList(line)
-            for word in word_list:
-                word_dict[word] = word_dict.get(word, 0) + 1
+    def get20MostFrequentWords(self):
+        word_dict = self.getWordDict()
 
         i, res = 0, []
         for k in sorted(word_dict, key=word_dict.get, reverse=True):
@@ -42,17 +57,40 @@ class Parser:
             i += 1
             if i >= 20:
                 break
-        
-        txt.close()
         return res
 
+
     def get20MostInterestingFrequentWords(self, no_common=100):
-        txt = open(self.filename, "r")
+        word_dict = self.getWordDict()
+        freq_word = util.getTopFrequence(no_common)
 
-        txt.close()
-        return
+        i, res = 0, []
+        for k in sorted(word_dict, key=word_dict.get, reverse=True):
+            if k in freq_word:
+                continue
+            res.append([k, word_dict[k]])
+            i += 1
+            if i >= 20:
+                break
+        return res
 
-p = Parser('./1342.txt')
-print(p.getTotalNumberOfWords())
-print(p.getTotalUniqueWords())
-print(p.get20MostFrequentWords())
+
+    def get20LeastFrequentWords(self):
+        word_dict = self.getWordDict()
+
+        i, res = 0, []
+        for k in sorted(word_dict, key=word_dict.get):
+            res.append([k, word_dict[k]])
+            i += 1
+            if i >= 20:
+                break
+        return res
+
+
+if __name__ == "__main__":
+    p = Parser('./1342.txt')
+    print(p.getTotalNumberOfWords())
+    print(p.getTotalUniqueWords())
+    print(p.get20MostFrequentWords())
+    print(p.get20MostInterestingFrequentWords(200))
+    print(p.get20LeastFrequentWords())
--- util.py
+++ util.py
@@ -1,2 +1,15 @@
 def getWordList(line):
     return line.strip().split()
+
+def getTopFrequence(top=100):
+    freq = open('./freq.txt', "r")
+
+    cnt, word_set = 0, set()
+    for line in freq:
+        word_set.add(line.strip())
+        cnt += 1
+        if cnt >= top:
+            break
+
+    freq.close()
+    return word_set
